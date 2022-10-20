b397847db52c37c5cc95dc0ddee1f76a1bd1f1dd
davydovMikhail <davydov27977@gmail.com>
2022-10-20 20:15:33 +0700

    test 2

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,9 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  gasReporter: {
+    enabled: true
+  },
 };
 
 export default config;
--- test/distribution.test.ts
+++ test/distribution.test.ts
@@ -10,7 +10,7 @@ describe("Exchanger test", async () => {
 
     let token: Token, distribution: Distribution;
     let admin: SignerWithAddress, user1: SignerWithAddress, user2: SignerWithAddress, 
-    user3: SignerWithAddress, user4: SignerWithAddress, user5: SignerWithAddress;
+    user3: SignerWithAddress, user4: SignerWithAddress, user5: SignerWithAddress, otherUser: SignerWithAddress;
     let arrayAddresses: string[];
 
     const defaultETHBalance = parseEther('10000');
@@ -23,7 +23,7 @@ describe("Exchanger test", async () => {
     let arrayAmountsETH: IArrEl;
 
     beforeEach(async () => {
-        [admin, user1, user2, user3, user4, user5] = await ethers.getSigners();
+        [admin, user1, user2, user3, user4, user5, otherUser] = await ethers.getSigners();
     });
 
     
@@ -61,6 +61,46 @@ describe("Exchanger test", async () => {
         await token.connect(admin).approve(distribution.address, parseEther('100000'));
     });
 
+    mocha.step("Check AccessControl", async function () {
+        arrayAddresses = [
+            user1.address,
+            user2.address,
+            user3.address,
+            user4.address,
+            user5.address
+        ]; 
+        const arrayAmounts = [
+            arrayAmountsToken[user1.address],
+            arrayAmountsToken[user2.address],
+            arrayAmountsToken[user3.address],
+            arrayAmountsToken[user4.address],
+            arrayAmountsToken[user5.address]
+        ];
+        await expect(distribution.connect(otherUser).distributeERC20(admin.address, token.address, arrayAddresses, arrayAmounts)).to.be.revertedWith("You do not have access rights.");
+    });
+
+    mocha.step("Checking for empty array", async function () {
+        const arrayAmounts = [
+            arrayAmountsToken[user1.address],
+            arrayAmountsToken[user2.address],
+            arrayAmountsToken[user3.address],
+            arrayAmountsToken[user4.address],
+            arrayAmountsToken[user5.address]
+        ];
+        const emptyArray: any = [];
+        await expect(distribution.connect(admin).distributeERC20(admin.address, token.address, emptyArray, arrayAmounts)).to.be.revertedWith("Empty array.");
+    });
+
+    mocha.step("Checking for length matching", async function () {
+        const arrayAmounts = [
+            arrayAmountsETH[user1.address],
+            arrayAmountsETH[user2.address],
+            arrayAmountsETH[user3.address],
+            arrayAmountsETH[user4.address]
+        ];
+        await expect(distribution.connect(admin).distributeERC20(admin.address, token.address, arrayAddresses, arrayAmounts)).to.be.revertedWith('Array lengths do not match.');
+    });
+
     mocha.step("Call distributeERC20", async function () {
         arrayAddresses = [
             user1.address,
@@ -85,6 +125,50 @@ describe("Exchanger test", async () => {
         expect(await token.balanceOf(user3.address)).to.equal(arrayAmountsToken[user3.address]);
         expect(await token.balanceOf(user4.address)).to.equal(arrayAmountsToken[user4.address]);
         expect(await token.balanceOf(user5.address)).to.equal(arrayAmountsToken[user5.address]);
+    });
+
+    mocha.step("Sending 0 ETH", async function () {
+        const arrayAmounts = [
+            arrayAmountsETH[user1.address],
+            arrayAmountsETH[user2.address],
+            arrayAmountsETH[user3.address],
+            arrayAmountsETH[user4.address],
+            arrayAmountsETH[user5.address]
+        ];
+        await expect(distribution.connect(admin).distributeETH(arrayAddresses, arrayAmounts, {value: 0})).to.be.revertedWith('You sent 0 ETH');
+    });
+
+    mocha.step("Checking for empty array", async function () {
+        const arrayAmounts = [
+            arrayAmountsETH[user1.address],
+            arrayAmountsETH[user2.address],
+            arrayAmountsETH[user3.address],
+            arrayAmountsETH[user4.address],
+            arrayAmountsETH[user5.address]
+        ];
+        const emptyArray: any = [];
+        await expect(distribution.connect(admin).distributeETH(emptyArray, arrayAmounts, {value: parseEther('70')})).to.be.revertedWith('Empty array.');
+    });
+
+    mocha.step("Checking for length matching", async function () {
+        const arrayAmounts = [
+            arrayAmountsETH[user1.address],
+            arrayAmountsETH[user2.address],
+            arrayAmountsETH[user3.address],
+            arrayAmountsETH[user4.address]
+        ];
+        await expect(distribution.connect(admin).distributeETH(arrayAddresses, arrayAmounts, {value: parseEther('70')})).to.be.revertedWith('Array lengths do not match.');
+    });
+
+    mocha.step("Checking of accounting for sent ether", async function () {
+        const arrayAmounts = [
+            arrayAmountsETH[user1.address],
+            arrayAmountsETH[user2.address],
+            arrayAmountsETH[user3.address],
+            arrayAmountsETH[user4.address],
+            arrayAmountsETH[user5.address]
+        ];
+        await expect(distribution.connect(admin).distributeETH(arrayAddresses, arrayAmounts, {value: parseEther('100')})).to.be.revertedWith('You have sent more ETH than needed');
     });
 
     mocha.step("Call distributeETH", async function () {
